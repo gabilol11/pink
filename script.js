96721578cecd77d9160be3ed2ebade5c39daffe8
gabilol11 <154045579+gabilol11@users.noreply.github.com>
2023-12-17 10:54:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+/*let name = "Name";
+function funcName (parame){
+    console.log("Hello",name);
+}
+funcName(varname);\
+*/
+
+// let human = {
+//      fullname: "Object Name",
+//      age: 11,
+//      email: "email.com"
+// }
+// console.log(human.fullname);
+
+// let dog = {
+//    dogname: "Georgel"
+//    color: "Cafeniu"
+//    age: 1.5
+// }
+// console.log(dog);
+
+// dog.age = 2.5;
+// dog.color = "Sur";
+
+// console.log(dog);
+
+
+
+
+// elemtele/functii obiect CSSLayerStatementRule
+
+// class human{
+//     let organe = {
+//         inima: "Transporta singe",
+//         creierul"Prelucreaza info"
+//     }
+
+
+// let info = "ndahkjv";
+
+// let n = {
+//     info:"Lesson 4",
+//     display(parametru){
+//         console.log(this,info,parametru);
+//     }
+// }
+
+// n.display("Text");
+
+
+
+
+
+
+
+// let phone = {
+//     brand: "Samsung",
+//     color: "black",
+//     procesor: "Snapdragon"
+//     camera:108,
+//     ac_dc: "este",
+
+//     turn0n(){
+//         console.log(this.brand, "se porneste");
+//     },
+//     discharge(){
+//         console.log("Rechearge me wai ... 1%");
+//     },
+//     call(){
+//         console.log("eu pot suna");
+//     },
+//     photo(){
+//         return"Eu fac poze in",this.camera,"MP";
+//     }
+
+// };
+
+// console.log(phone.brand);
+// console.log(phone.photo());
+// phone.call();
+
+
+// class cat{
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }ceaipitea(cat1, cat2){
+//         console.log(cat1, "a venit la",
+//         cat2, "sa bea ceai");
+//     }
+// }
+
+// let barsik = new cat("Barsik", 2);
+// let garfild =new cat ("Garfild", 3);
+// barsik.ceaipitea(garfild.name);
+// garfild.ceaipitea(barsik.name);
